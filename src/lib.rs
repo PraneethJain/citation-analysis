a69238564d97086551341a520564566cdb90267e
PraneethJain <praneethjain005@gmail.com>
2024-02-13 17:09:08 +0530

    refactor: save_graphs

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,10 @@ fn dates() -> impl Iterator<Item = String> {
 }
 
 pub fn save_graphs(graphs: &Graphs) {
-    for year in 1992..=2002 {
-        for month in 1..=12 {
-            let g = graphs.till(&Date::from(&format!("{:04}-{:02}-01", year, month)));
-            graphviz::save(&format!("graphs/g{:04}-{:02}.gv", year, month), &g);
-        }
-    }
+    dates().for_each(|date_str| {
+        let filename = String::from("graphs/g") + &date_str[..7] + ".gv";
+        graphviz::save(&filename, &graphs.till(&Date::from(&date_str)));
+    });
 }
 
 pub fn save_scc_counts(filename: &str, graphs: &Graphs) {
